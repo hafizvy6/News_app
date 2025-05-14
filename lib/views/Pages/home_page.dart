9da8deba7ed12373112c03f7503f9f38bea838be
Mohamed Hafiz <m.hafizvy6@gmail.com>
2025-05-14 18:47:45 +0400

    Fixes and updates

--- lib/views/Pages/home_page.dart
+++ lib/views/Pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/widgets/category_list.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -19,7 +20,7 @@ class HomePage extends StatelessWidget {
           ],
         ),
       ),
-      body: Center(child: Text('Home Page')),
+      body: CategoryList(),
     );
   }
 }
--- lib/widgets/category_list.dart
+++ lib/widgets/category_list.dart
@@ -3,8 +3,41 @@ import 'package:flutter/material.dart';
 class CategoryList extends StatelessWidget {
   const CategoryList({super.key});
 
+  final List<String> categories = const [
+    "All news",
+    "Business",
+    "Politics",
+    "Tech",
+    "Healthy",
+    "Science",
+    "Entertainment",
+    "World",
+    "Nature",
+    "International",
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SizedBox(
+      height: 45,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.grey[200],
+                foregroundColor: Colors.black,
+                shape: StadiumBorder(),
+                elevation: 0,
+              ),
+              child: Text(categories[index]),
+            ),
+          );
+        },
+      ),
+    );
   }
 }
