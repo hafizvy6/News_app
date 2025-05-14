30a6ac8f33eca791b846a1f26fd57b6b3918250e
Mohamed Hafiz <m.hafizvy6@gmail.com>
2025-05-14 18:00:24 +0400

    Fixes and updates

--- lib/views/Pages/home_page.dart
+++ lib/views/Pages/home_page.dart
@@ -5,6 +5,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Text('Home Page'));
+    return Scaffold(appBar: AppBar(), body: Text('Home Page'));
   }
 }
